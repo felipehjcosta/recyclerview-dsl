202ebec87044050329683cb82babe144f88e4ea7
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-06-10 22:14:55 -0300

    Project setup

---
+++ sample/src/main/java/com/github/felipecosta/recyclerviewdsl/sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.github.felipecosta.recyclerviewdsl.sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.github.felipecosta.recyclerviewdsl.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
