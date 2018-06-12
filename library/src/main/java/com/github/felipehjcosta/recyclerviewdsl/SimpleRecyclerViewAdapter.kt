03e039d47ffdba04a78a3a33db0c26349897b969
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-06-12 09:11:35 -0300

    Use a default view type for item

--- library/src/main/java/com/github/felipehjcosta/recyclerviewdsl/SimpleRecyclerViewAdapter.kt
+++ library/src/main/java/com/github/felipehjcosta/recyclerviewdsl/SimpleRecyclerViewAdapter.kt
@@ -14,17 +14,21 @@ internal class SimpleRecyclerViewAdapter(
     override fun getItemCount(): Int = items.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleRecyclerView {
-        val view = LayoutInflater.from(parent.context).inflate(layoutBinds.keyAt(0), parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(layoutBinds.keyAt(viewType), parent, false)
         return SimpleRecyclerView(view)
     }
 
     override fun onBindViewHolder(holder: SimpleRecyclerView, position: Int) {
-        val bindMap = layoutBinds.valueAt(0).bindMap
+        val bindMap = layoutBinds.valueAt(holder.itemViewType).bindMap
         val item = items[position]
         val id = bindMap.keyAt(0)
         val view = holder.itemView.findViewById<View>(id)
         val block = bindMap.valueAt(0)
         block(item, view)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return 0
     }
 
     internal class SimpleRecyclerView(view: View) : RecyclerView.ViewHolder(view)
