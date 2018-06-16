542b392d8d86e029fd4404a4c7191cf3e2be450a
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-06-16 00:48:49 -0300

    Extract sample to a separate fragment

--- sample/src/main/java/com/github/felipecosta/recyclerviewdsl/sample/MainActivity.kt
+++ sample/src/main/java/com/github/felipecosta/recyclerviewdsl/sample/MainActivity.kt
@@ -2,11 +2,8 @@ package com.github.felipecosta.recyclerviewdsl.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.RecyclerView
-import android.widget.LinearLayout
-import android.widget.TextView
 import com.github.felipecosta.recyclerviewdsl.R
-import com.github.felipehjcosta.recyclerviewdsl.onRecyclerView
+import com.github.felipecosta.recyclerviewdsl.sample.strings.StringsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,34 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-
-        val titles = listOf(
-                "Spider-Man",
-                "Thor",
-                "Iron Main",
-                "Black Panther",
-                "Black Widow",
-                "Captain America",
-                "Captain Marvel",
-                "Falcon",
-                "Hank Pym",
-                "Hawkeye",
-                "Hulk")
-
-        onRecyclerView(recyclerView) {
-            withLinearLayout {
-                orientation = LinearLayout.VERTICAL
-                reverseLayout = false
-            }
-
-            withItems(titles) {
-                bind(R.layout.recycler_view_item) {
-                    on<String, TextView>(R.id.title) {
-                        it.view?.text = it.item
-                    }
-                }
-            }
-        }
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.main_content, StringsFragment.newInstance())
+                .commit()
     }
 }
---
+++ sample/src/main/java/com/github/felipecosta/recyclerviewdsl/sample/strings/StringsFragment.kt
@@ -0,0 +1,58 @@
+package com.github.felipecosta.recyclerviewdsl.sample.strings
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.github.felipecosta.recyclerviewdsl.R
+import com.github.felipehjcosta.recyclerviewdsl.onRecyclerView
+
+class StringsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.strings_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+
+        val titles = listOf(
+                "Spider-Man",
+                "Thor",
+                "Iron Main",
+                "Black Panther",
+                "Black Widow",
+                "Captain America",
+                "Captain Marvel",
+                "Falcon",
+                "Hank Pym",
+                "Hawkeye",
+                "Hulk")
+
+        onRecyclerView(recyclerView) {
+            withLinearLayout {
+                orientation = LinearLayout.VERTICAL
+                reverseLayout = false
+            }
+
+            withItems(titles) {
+                bind(R.layout.recycler_view_item) {
+                    on<String, TextView>(R.id.title) {
+                        it.view?.text = it.item
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        fun newInstance() = StringsFragment()
+    }
+}
