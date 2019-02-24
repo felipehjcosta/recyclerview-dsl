94276b54342745f93117bfa8c69c490dfe04b567
felipehjcosta <felipe.h.j.costa@gmail.com>
2019-02-24 19:04:50 -0300

    Move commands from buildViaTravis.sh to CI yml

--- gradle/buildViaTravis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_TAG" != "" ]; then
-  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew clean build lib:jacocoTestReportDebug bintrayUpload -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" -PdryRun=false --console=plain
-else
-  echo -e 'Normal Build => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew clean build lib:jacocoTestReportDebug --console=plain
-fi
